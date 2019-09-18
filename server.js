56b05707ae8c758c31f78af789e07be44faca3a6
stefanliemawan <stefanliemawan@gmail.com>
2019-09-18 18:54:08 +0700

    realtime bitcoin tracker

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "864217",
+  key: "d39d3aae4dad99a024f8",
+  secret: "48e5bad348aa08cad885",
+  cluster: "ap1",
+  encrypted: true
+});
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// CORS middleware
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
+});
+
+// Set port to be used by Node.js
+app.set("port", 5000);
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+// API route in which the price information will be sent to from the clientside
+app.post("/prices/new", (req, res) => {
+  // Trigger the 'prices' event to the 'coin-prices' channel
+  pusher.trigger("coin-prices", "prices", {
+    prices: req.body.prices
+  });
+  res.sendStatus(200);
+});
+
+app.listen(app.get("port"), () => {
+  console.log("Node app is running on port", app.get("port"));
+});
---
+++ src/Price/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import axios from "axios";
+import Pusher from "pusher-js";
+import "./styles.css";
+
+export default class Price extends React.Component {
+  state = {
+    btcprice: ""
+  };
+
+  sendPricePusher(data) {
+    axios
+      .post("/prices/new", {
+        prices: data
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD"
+      )
+      .then(response => {
+        this.setState({ btcprice: response.data.BTC.USD });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    this.pusher = new Pusher("d39d3aae4dad99a024f8", {
+      cluster: "ap1",
+      encrypted: true,
+      forceTLS: true
+    });
+    this.prices = this.pusher.subscribe("coin-prices");
+    setInterval(() => {
+      axios
+        .get(
+          "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD"
+        )
+        .then(response => {
+          this.sendPricePusher(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, 10000);
+    this.prices.bind(
+      "prices",
+      price => {
+        this.setState({ btcprice: price.prices.BTC.USD });
+      },
+      this
+    );
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h2>Current Price</h2>
+        <h1>${this.state.btcprice}</h1>
+        <p>1 BTC</p>
+      </div>
+    );
+  }
+}
