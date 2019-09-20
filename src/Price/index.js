7dfb3b0da888b6e43fa252c233d0fca3b7266566
stefanliemawan <stefanliemawan@gmail.com>
2019-09-20 15:16:47 +0700

    improved performance and setup

--- src/Price/index.js
+++ src/Price/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import Pusher from "pusher-js";
-import HttpsRedirect from "react-https-redirect";
 import Input from "./Input";
 import "./styles.css";
 
@@ -26,7 +25,7 @@ export default class Price extends React.Component {
   componentDidMount() {
     axios
       .get(
-        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD"
+        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD"
       )
       .then(response => {
         this.setState({ btcprice: response.data.BTC.USD });
@@ -47,7 +46,7 @@ export default class Price extends React.Component {
     setInterval(() => {
       axios
         .get(
-          "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD"
+          "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD"
         )
         .then(response => {
           this.sendPricePusher(response.data);
@@ -67,14 +66,12 @@ export default class Price extends React.Component {
 
   render() {
     return (
-      <HttpsRedirect>
-        <div className="container">
-          <h2>Current Price</h2>
-          <h1>${this.state.btcprice}</h1>
-          <p>1 BTC</p>
-          <Input price={this.state.btcprice} />
-        </div>
-      </HttpsRedirect>
+      <div className="container">
+        <h2>Current Price</h2>
+        <h1>${this.state.btcprice}</h1>
+        <p>1 BTC</p>
+        <Input price={this.state.btcprice} />
+      </div>
     );
   }
 }
